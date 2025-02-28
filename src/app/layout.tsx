794044effd7a69f2a9f4791909b9914b611b79b0
Amar Pandey <amarpandey2502@gmail.com>
2025-02-28 11:47:41 +0530

    changed the layout for additional font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "@/components/ui/sonner"
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
   title: "CodeVault",
@@ -24,12 +13,11 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-      <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          {children}
-          <Toaster />
-        </body>
-      </html>
+    <html lang="en">
+      <body className="antialiased">
+        {children}
+        <Toaster />
+      </body>
+    </html>
   );
 }
