d3d63061409bf1fc6bca2c8e0bdf18059055d033
Amar Pandey <amarpandey2502@gmail.com>
2025-03-02 11:19:55 +0530

    made index.d.ts for session interface

---
+++ index.d.ts
@@ -0,0 +1,23 @@
+import 'next-auth';
+
+type Project = {
+    project_name: string,
+    project_description: string,
+    files: {
+      html: string,
+      css: string,
+      javascript: string
+    }
+  }
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      id: string;
+      name: string;
+      email: string;
+      image?: string;
+      projects?: Project[]; 
+    }
+  }
+}
